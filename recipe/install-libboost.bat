84795d2f67971a7a952a9eb93fd47206d99cbab6
Ray Donnelly <mingw.android@gmail.com>
2017-10-18 10:48:19 -0700

    (WIP) pinging @msarahan
    Need to see why install-libboost.bat gives:
    
    C:\Users\builder\m64\conda-bld\libboost_1508360810013\work>"C:\Users\builder\m64\conda-bld\libboost_1508360810013\_h_env\"Scripts\activate "C:\Users\builder\m64\conda-bld\libboost_1508360810013\_h_env"
    "Did not find VS in registry or in VS90COMNTOOLS env var - exiting"

---
+++ recipe/install-libboost.bat
@@ -0,0 +1,26 @@
+"%PREFIX%\"Scripts\activate "%PREFIX%"
+
+./b2 -q ^
+     install > b2.install.log 2>&1
+
+:: Get the major_minor_patch version info, e.g. `1_61_1`. In
+:: the past this has been just major_minor, so we do not just
+:: replace all dots with underscores in case it changes again
+for /F "tokens=1,2,3 delims=." %%a in ("%PKG_VERSION%") do (
+   set MAJ=%%a
+   set MIN=%%b
+   set PAT=%%c
+)
+set MAJ_MIN_PAT_VER=%MAJ%_%MIN%_%PAT%
+
+:: Install fix-up for a non version-specific boost include
+move %LIBRARY_INC%\boost-%MAJ_MIN_PAT_VER%\boost %LIBRARY_INC%
+if errorlevel 1 exit /b 1
+
+:: Remove Python headers as we don't build Boost.Python.
+del %LIBRARY_INC%\boost\python.hpp
+rmdir /s /q %LIBRARY_INC%\boost\python
+
+:: Move DLLs to LIBRARY_BIN
+move %LIBRARY_LIB%\*vc%VS_MAJOR%0-mt-%MAJ_MIN_PAT_VER%.dll "%LIBRARY_BIN%"
+if errorlevel 1 exit /b 1
---
+++ recipe/install-py-boost.bat
@@ -0,0 +1,7 @@
+"%PREFIX%"\Scripts\activate.bat "%PREFIX%"
+
+./b2 -q ^
+      python=%PY_VER% ^
+      -j%CPU_COUNT% ^
+      --with-python ^
+      install > b2.install-py-%PY_VER%.log 2>&1
