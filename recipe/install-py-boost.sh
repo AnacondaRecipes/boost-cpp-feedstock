15fa017a382a7e9d8a59581bdc0d7739ee22e6b6
Ray Donnelly <mingw.android@gmail.com>
2017-10-22 19:35:06 +0100

    Python2's boost.python is called libboost_python.so

--- recipe/install-py-boost.sh
+++ recipe/install-py-boost.sh
@@ -12,9 +12,11 @@ set -x -e
      install | tee b2.install-py-${PY_VER}.log 2>&1
 
 # boost.python, when driven via bjam always links to boost_python
-# instead of boost_python2 or boost_python3. It also does not add
-# any passed --python-buildid; ping @stefanseefeld
-pushd "${PREFIX}/lib"
-  ln -s libboost_python${PY_VER%.*}.so libboost_python.so
-  ln -s libboost_numpy${PY_VER%.*}.so libboost_numpy.so
-popd
+# instead of boost_python3. It also does not add any specified
+# --python-buildid; ping @stefanseefeld
+if [[ ${PY_VER%.*} == 3 ]]; then
+  pushd "${PREFIX}/lib"
+    ln -s libboost_python${PY_VER%.*}.so libboost_python.so
+    ln -s libboost_numpy${PY_VER%.*}.so libboost_numpy.so
+  popd
+fi
